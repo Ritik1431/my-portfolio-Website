75b25773cfcb711ec1bd2a6b2b934d109cf2e413
Ritik1431 <ritik221jaggi@gmail.com>
2023-01-28 01:17:34 +0530

    wef

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,14 +3,14 @@ import './Experience.css'
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>What skills I Have</h5>
+      <h5>What skills I Have ghgdtfjgtmnhgm </h5>
       <h2>My Experience</h2>
       <div className="container experience_container">
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
             <article className='experience_details'>
-              
+
             </article>
           </div>
         </div>
