2fda039c906b370ec5eaa9aa97411b65bfb9d5f8
Ritik1431 <ritik221jaggi@gmail.com>
2023-01-29 13:39:03 +0530

    done

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from'../../Assets/portfolio1.jpg'
+import IMG2 from'../../Assets/portfolio2.jpg'
+import IMG3 from'../../Assets/portfolio3.jpg'
+import IMG4 from'../../Assets/portfolio4.jpg'
+import IMG5 from'../../Assets/portfolio5.png'
+import IMG6 from'../../Assets/portfolio6.jpg'
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio_container">
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG1} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG2} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG3} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG4} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG5} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+        <article className="portfolio_item">
+          <div className="portfolio_item-image">
+            <img src={IMG6} alt="" />
+            <h3>This is a portfolio title</h3>
+            <div className="portfolio_item-btn">
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://dribble.com" className='btn btn-primary' target='blank'>Live Demo</a>
+            </div>
+          </div>
+        </article>
+      </div>
     </section>
   )
 }
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,97 @@
 import React from 'react'
 import './Services.css'
+import {BsCheckLg} from 'react-icons/bs'
 const Services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+    
+    <div className="container services_container">
+      <article className="services">
+        <div className="service_head">
+          <h3>UI/UX Design</h3>
+        </div>
+        <ul className="service_list">
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+        </ul>
+      </article>
+      {/* {End of UI/UX} */}
+      <article className="services">
+        <div className="service_head">
+          <h3>Data Science</h3>
+        </div>
+        <ul className="service_list">
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+        </ul>
+      </article>
+      {/* {End of Data Science} */}
+      <article className="services">
+        <div className="service_head">
+          <h3>Other Skills</h3>
+        </div>
+        <ul className="service_list">
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            <BsCheckLg className='service_list-icon'/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+        </ul>
+      </article>
+    </div>
     </section>
   )
 }
