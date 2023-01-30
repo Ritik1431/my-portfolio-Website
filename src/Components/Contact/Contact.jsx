c732ee01ae1527fbbc748a7c3c4cf0e240014f41
Ritik1431 <ritik221jaggi@gmail.com>
2023-01-31 00:30:29 +0530

    done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './Contact.css';
+import {MdEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_option">
+          <MdEmail/>
+          <h4>Email</h4>
+          <h5>ritik221jaggi@gmail.com</h5>
+          <a href="mailto:ritik221jaggi@gmail.com">Send a message</a>
+        </div>
+        <div className="contact_option">
+        <BsWhatsapp/>
+          <h4>WhatsApp</h4>
+          <h5>+917888924188</h5>
+          <a href="https://api.whatsapp.com/send?phone+917888924188">Send a message</a>
+        </div>
+        <div className="contact_option">
+        <BsLinkedin/>
+          <h4>LinkedIn</h4>
+          <h5>ritik221jaggi@gmail.com</h5>
+          <a href="linkedin.com">Send a message</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,72 +1,74 @@
-import React from 'react'
-import './Testimonials.css'
-import {BsPersonCircle} from 'react-icons/bs'
-import AVTR1 from '../../Assets/prnv.jpg'
-import AVTR2 from '../../Assets/khushu.jpg'
-import AVTR3 from '../../Assets/sukham.jpg'
-import AVTR4 from '../../Assets/swastik.jpg'
+import React from "react";
+import "./Testimonials.css";
+import { BsPersonCircle } from "react-icons/bs";
+import AVTR1 from "../../Assets/prnv.jpg";
+import AVTR2 from "../../Assets/khushu.jpg";
+import AVTR3 from "../../Assets/sukham.jpg";
+import AVTR4 from "../../Assets/swastik.jpg";
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Pranav Tyagi",
+    review:
+      "Its my privilegde to work with you.It was great experience.This site helped me a lot to learn the concepts.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Khushal Mehta",
+    review:
+      "Its my privilegde to work with you.It was great experience.This site helped me a lot to learn the concepts.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Sukham",
+    review:
+      "Its my privilegde to work with you.It was great experience.This site helped me a lot to learn the concepts.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Swastik Gupta",
+    review:
+      "Its my privilegde to work with you.It was great experience.This site helped me a lot to learn the concepts.",
+  },
+];
 const Testimonials = () => {
   return (
-    <section id='testimonials'>
+    <section id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials_container">
-        <article className="testimonials">
-          <div className="client_avatar">
-            <img src={AVTR1} alt="Avatar" />
-          </div>
-          <div className="client_name-icon">
-            <BsPersonCircle className='client-icon'/>
-            <h5 className='client_name'>Pranav Tyagi</h5>
-          </div>
-            <small className="client_review">
-              Its my privilegde to work with you.It was great experience.
-              This site helped me a lot to learn the concepts.
-            </small>
-        </article>
-        <article className="testimonials">
-          <div className="client_avatar">
-            <img src={AVTR2} alt="Avatar" />
-          </div>
-          <div className="client_name-icon">
-            <BsPersonCircle className='client-icon'/>
-            <h5 className='client_name'>Khushal Mehta</h5>
-          </div>
-            <small className="client_review">
-              Its my privilegde to work with you.It was great experience.
-              This site helped me a lot to learn the concepts.
-            </small>
-        </article>
-        <article className="testimonials">
-          <div className="client_avatar">
-            <img src={AVTR3} alt="Avatar" />
-          </div>
-          <div className="client_name-icon">
-            <BsPersonCircle className='client-icon'/>
-            <h5 className='client_name'>Sukham</h5>
-          </div>
-            <small className="client_review">
-              Its my privilegde to work with you.It was great experience.
-              This site helped me a lot to learn the concepts.
-            </small>
-        </article>
-        <article className="testimonials">
-          <div className="client_avatar">
-            <img src={AVTR4} alt="Avatar" />
-          </div>
-          <div className="client_name-icon">
-            <BsPersonCircle className='client-icon'/>
-            <h5 className='client_name'>Swastik gupta</h5>
-          </div>
-            <small className="client_review">
-              Its my privilegde to work with you.It was great experience.
-              This site helped me a lot to learn the concepts.
-            </small>
-        </article>
-        
-      </div>
+      <Swiper className="container testimonials_container"
+    // install Swiper modules
+    modules={[Pagination]}
+    spaceBetween={40}
+    slidesPerView={1}
+    pagination={{ clickable: true }}>
+        {
+        data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonials">
+              <div className="client_avatar">
+                <img src={avatar} alt="Avatar" />
+              </div>
+              <div className="client_name-icon">
+                <BsPersonCircle className="client-icon" />
+                <h5 className="client_name">{name}</h5>
+              </div>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
