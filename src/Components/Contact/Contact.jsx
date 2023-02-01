c468163797b8eea6bb53f4271e149e3ff76fa6f8
Ritik1431 <ritik221jaggi@gmail.com>
2023-02-01 14:17:41 +0530

    done

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,13 +6,12 @@ import {BsLinkedin} from 'react-icons/bs'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
 const Contact = () => {
-  // const form = useRef();
-  // emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-  // .then((result) => {
-  //     console.log(result.text);
-  // }, (error) => {022
-  //     console.log(error.text);
-  // });
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ulk83gt', 'template_qfbkjsn', form.current, '0nqge-yVcMCLr7iP1')
+  }
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -40,7 +39,7 @@ const Contact = () => {
         </article>
         </div>
         {/* {End of Contact Options} */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message'></textarea>
